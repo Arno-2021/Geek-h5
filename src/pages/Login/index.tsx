e4d5a48fbab1063f5a15b0db9b676ed5d7ce6cd8
Arno-2021 <aa307204890@gmail.com>
2021-11-27 17:18:04 +0800

    login form validate

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -10,15 +10,38 @@ export default function Login() {
             <div className='login-form'>
                 <h2 className='title'>账号登录</h2>
 
-                <Form>
-                    <Form.Item className='login-item'>
-                        <Input placeholder='请输入用户名'></Input>
+                <Form validateTrigger={['onBlur', 'onChange']}>
+                    <Form.Item
+                        className='login-item'
+                        name='mobile'
+                        rules={[
+                            { required: true, message: '请输入手机号' },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '请输入正确的手机号',
+                            },
+                        ]}
+                    >
+                        <Input
+                            placeholder='请输入手机号'
+                            maxLength={11}
+                        ></Input>
                     </Form.Item>
                     <List.Item
                         className='login-code-extra'
                         extra={<span className='code-extra'>发送验证码</span>}
                     >
-                        <Form.Item className='login-item'>
+                        <Form.Item
+                            className='login-item'
+                            name='code'
+                            rules={[
+                                { required: true, message: '请输入验证码' },
+                                {
+                                    pattern: /^\d{6}$/,
+                                    message: '请输入6位验证码',
+                                },
+                            ]}
+                        >
                             <Input placeholder='请输入验证码'></Input>
                         </Form.Item>
                     </List.Item>
